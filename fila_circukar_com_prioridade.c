68f6346f338e0d44a7cdd2e7c3de2efa986bdfdb
Gabriel Louzada da Silva <131915775+Louzada27@users.noreply.github.com>
2023-04-27 18:23:52 -0300

    Update fila_circukar_com_prioridade.c

--- fila_circukar_com_prioridade.c
+++ fila_circukar_com_prioridade.c
@@ -1,13 +1,3 @@
-- 👋 Hi, I’m @Louzada27
-- 👀 I’m interested in ...
-- 🌱 I’m currently learning ...
-- 💞️ I’m looking to collaborate on ...
-- 📫 How to reach me ...
-
-<!---
-Louzada27/Louzada27 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
 #include <stdio.h>
 #include <stdlib.h>
 
